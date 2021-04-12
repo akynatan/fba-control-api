9bb469cf6f4b95f2358abb4bc3526012d2593534
Aky Natan <akynatan.a@gmail.com>
2021-04-12 17:32:06 -0300

    cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -17,13 +17,7 @@ import '@shared/container';
 
 const app = express();
 
-app.use(
-  cors({
-    allowedHeaders: ['Content-Type'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    origin: 'https://homologacao.d3k85ql4gcjt4c.amplifyapp.com/',
-  }),
-);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(rateLimiter);
