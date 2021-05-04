d253e8772235acd917df44b641b35d4e3ef47896
Aky Natan <akynatan.a@gmail.com>
2021-05-04 12:22:34 -0300

    commit terca

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -35,7 +35,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   console.error(err);
   return response.status(500).json({
     status: 'error',
-    error: 'Internal server error..',
+    error: 'Internal server error.',
   });
 });
 
