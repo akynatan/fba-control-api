a4917a5b28ccc1bc790cc5f71aa2db391d3b27eb
Aky Natam <akynatan@outlook.com>
2021-07-11 15:32:38 -0300

    Iniciando os trabalhos - 15:30 - Criando logs_routines

---
+++ src/shared/infra/typeorm/migrations/1626028350001-CreateLogRoutines.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateLogRoutines1626028350001 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
